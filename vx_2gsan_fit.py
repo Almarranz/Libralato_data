f648544ca31d14926638b7b1c0e257b014fc857a
Alvaro Martinez <amartinez@iaa.es>
2022-02-10 15:33:25 +0100

    Added several feartures to all of them. The way of calculating the uncertainties for the PM in galactic coordinates is not correct. The way to transform PM to galactic seems IT IS.

--- vx_2gsan_fit.py
+++ vx_2gsan_fit.py
@@ -43,14 +43,14 @@ cata='/Users/amartinez/Desktop/PhD/Libralato_data/CATALOGS/'
 
 #R.A. Dec. X Y μαcosδ σμαcosδ μδ σμδ  time n1 n2 ID
 
-# name='ACSWFC'
-name='WFC3IR'
+name='ACSWFC'
+# name='WFC3IR'
 ra,dec,x_c ,y_c,mua,dmua,mud,dmud, time, n1, n2, idt = np.loadtxt(cata+'GALCEN_%s_PM.cat'%(name),unpack=True)
 # VEGAmag, rmsmag, QFIT, o, RADXS, nf, nu, Localsky, Local-skyrms
 mag, rms, qfit, o, RADXS, nf, nu, Localsky, Local_skyrms= np.loadtxt(cata+'GALCEN_%s_GO12915.cat'%(name),unpack=True )
 
 #%%
-good=np.where((dmua<90)&(dmua<5))
+good=np.where((dmua<90))
 ra=ra[good]
 dec=dec[good]
 mua=mua[good]
@@ -89,14 +89,9 @@ dmub=np.array(dmub)
 # =============================================================================
 #%%
 v_lim=70
-# good=np.where((mul<v_lim) & (mul>-v_lim))
-good=np.where((mua<v_lim) & (mua>-v_lim))
+good=np.where((mul<v_lim) & (mul>-v_lim))
 ra=ra[good]
 dec=dec[good]
-mua=mua[good]
-mud=mud[good]
-dmua=dmua[good]
-dmud=dmud[good]
 mul=mul[good]
 dmul=dmul[good]
 mub=mub[good]
@@ -106,12 +101,11 @@ n1=n1[good]
 n2=n2[good]
 idt=idt[good]
 #%%
-perc_dmul= np.percentile(dmua,85)#this is the way they do it in the paper(i thing), but the uncertainty is too high
+perc_dmul= np.percentile(dmul,85)#this is the way they do it in the paper(i thing), but the uncertainty is too high
 print(perc_dmul,'yomama')
-lim_dmul=perc_dmul
-# lim_dmul=0.7
-# accu=np.where((abs(dmul)<lim_dmul) & (abs(dmub)<lim_dmul))#Are they in the paper selecting by the error of the galactic or equatorial coordintes???
-accu=np.where((abs(dmua)<lim_dmul) & (abs(dmud)<lim_dmul))
+# lim_dmul=perc_dmul
+lim_dmul=2
+accu=np.where((abs(dmul)<lim_dmul) & (abs(dmub)<lim_dmul))
 #%%
 mul=mul[accu]
 mub=mub[accu]
@@ -120,10 +114,10 @@ dmub=dmub[accu]
 time=time[accu]
 #%%
 print(min(mul),max(mul))
-binwidth=0.25
+
 auto='no'
 if auto !='auto':
-    auto=np.arange(min(mul),max(mul)+ binwidth, binwidth)#also works if running each bing width one by one, for some reason...
+    auto=np.arange(min(mul),max(mul),0.25)#also works if running each bing width one by one, for some reason...
     # print(auto)
 
 #%%
@@ -176,8 +170,8 @@ def gaussian(x, mu, sig, amp):
     return amp * (1 / (sig * (np.sqrt(2 * np.pi)))) * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))) 
 #%
 def loglike(theta):
-    mu1, sigma1, amp1,mu2,sigma2,amp2,mu3,sigma3,amp3 = theta
-    model = gaussian(x, mu1, sigma1, amp1)+gaussian(x,mu2,sigma2,amp2)+gaussian(x,mu3,sigma3,amp3)
+    mu1, sigma1, amp1,mu2,sigma2,amp2 = theta
+    model = gaussian(x, mu1, sigma1, amp1)+gaussian(x,mu2,sigma2,amp2)
      
     return -0.5 * np.sum(((y - model)/yerr) ** 2)#chi squared model
 
@@ -188,25 +182,23 @@ def prior_transform(utheta):
     to the parameter of interest `x ~ Unif[-10., 10.)`."""
     #x = 2. * u - 1.  # scale and shift to [-1., 1.)
     #x *= 10.  # scale to [-10., 10.)
-    umu1, usigma1, uamp1,  umu2, usigma2, uamp2, umu3, usigma3, uamp3= utheta
+    umu1, usigma1, uamp1,  umu2, usigma2, uamp2= utheta
      
 #%     mu1 = -1. * umu1-8   # scale and shift to [-10., 10.)
-    mu1 = -3+ (5*umu1-5/2)  # yellow
-    sigma1 = 2* (usigma1)   
+    mu1 = -4+ (1*umu1-1/2)  # yellow
+    sigma1 = 2.5+ (1*umu1-1/2)   
     amp1 = 1 * uamp1 
    
-    mu2 = -5+ (6*umu2-6/2) # red
-    sigma2 = 2 * (2*usigma2-1)   
+    mu2 = -5+ (1*umu2-1/2) # red
+    sigma2 =  3.5+ (1*umu2-1/2)  
     amp2 = 1* uamp2   
 
-    mu3 = -7+ (4*umu3-4/2) # black
-    sigma3 = 3*(usigma3)
-    amp3 = 1*uamp3
+   
         
-    return mu1, sigma1, amp1, mu2, sigma2, amp2, mu3, sigma3, amp3
+    return mu1, sigma1, amp1, mu2, sigma2, amp2
     
 #%
-sampler = dynesty.NestedSampler(loglike, prior_transform, ndim=9, nlive=200,
+sampler = dynesty.NestedSampler(loglike, prior_transform, ndim=6, nlive=200,
                                             bound='multi', sample='rwalk')
 sampler.run_nested()
 res = sampler.results
@@ -220,7 +212,7 @@ labels = [r'$\mathrm{\mu 1}$', r'$\mathrm{\sigma 1}$', r'$amp1$', r'$\mathrm{\mu
     #                              fig=plt.subplots(6, 2, figsize=(16, 27)))
     
 fig, axes = dyplot.traceplot(sampler.results,labels=labels,show_titles=True,
-                                 fig=plt.subplots(9, 2, figsize=(16, 20)))
+                                 fig=plt.subplots(6, 2, figsize=(16, 20)))
     
 plt.show()
 
@@ -237,9 +229,9 @@ sp=np.ones(9)*0.68
 fig, axes = dyplot.cornerplot(res, color='royalblue' ,show_titles=True,quantiles=[0.16,0.5,0.84],truths=mean,
                                   quantiles_2d=[0.16,0.5,0.84],
                                   title_kwargs=({'x': 0.65, 'y': 1.05}), labels=labels,
-                                  fig=plt.subplots(9, 9, figsize=(28, 28)))
+                                  fig=plt.subplots(6, 6, figsize=(28, 28)))
 
-plt.legend(['$\mu_{l}$ %s'%(name)],fontsize=70,markerscale=0,shadow=True,bbox_to_anchor=(1.2,9.8),handlelength=-0.0)
+plt.legend(['$\mu_{l}$ %s'%(name)],fontsize=70,markerscale=0,shadow=True,bbox_to_anchor=(1,6.5),handlelength=-0.0)
 plt.show() 
 #% 
 
@@ -249,15 +241,14 @@ print(results['logz'][-1])
 # h=plt.hist(v_x*-1, bins= nbins, color='darkblue', alpha = 0.6, density =True, histtype = 'stepfilled')
 h=plt.hist(mul, bins= auto, color='royalblue', alpha = 0.6, density =True, histtype = 'stepfilled')
 
-xplot = np.linspace(min(x), max(x), 1000)
+xplot = np.linspace(min(x), max(x), 500)
 
 # plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) , color="darkorange", linewidth=3, alpha=0.6)
 
-plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) + gaussian(xplot, mean[3], mean[4], mean[5])
-         + gaussian(xplot, mean[6], mean[7], mean[8]), color="darkorange", linewidth=3, alpha=1)
+plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) + gaussian(xplot, mean[3], mean[4], mean[5]), color="darkorange", linewidth=3, alpha=1)
 plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2])  , color="yellow", linestyle='dashed', linewidth=3, alpha=0.6)
 plt.plot(xplot, gaussian(xplot, mean[3], mean[4], mean[5])  , color="red", linestyle='dashed', linewidth=3, alpha=0.6)
-plt.plot(xplot, gaussian(xplot, mean[6], mean[7], mean[8]) , color='black', linestyle='dashed', linewidth=3, alpha=0.6)
+
 plt.xlim(-15,3)
 plt.text(-10,max(h[0]-0.01),'logz=%.0f'%(results['logz'][-1]),color='b')
 
@@ -270,35 +261,20 @@ plt.xlabel(r'$\mathrm{\mu_{l} (mas\ yr^{-1})}$')
 samples, weights = res.samples, np.exp(res.logwt - res.logz[-1])
 mean, cov = dyfunc.mean_and_cov(samples, weights)
 # print(mean)
-quantiles = [dyfunc.quantile(samps, [0.16,0.5,.84], weights=weights)
+quantiles = [dyfunc.quantile(samps, [0.16,0.5,0.84], weights=weights)
              for samps in samples.T]
-for i in range(9):
+
+for i in range(6):
     print('medin %.2f -+ %.2f %.2f'%(quantiles[i][1],quantiles[i][1]-quantiles[i][0],quantiles[i][2]-quantiles[i][1]))
     print(' mean %.2f -+ %.2f %.2f'%(mean[i],mean[i]-quantiles[i][0],quantiles[i][2]-mean[i])+'\n'+30*'*')
 
 
 
-# %%
 
 
 
 
-# =============================================================================
-# bs = np.arange(min(mul), max(mul), 0.25)
-# hist, edges = np.histogram(mul, bs)
-# freq = hist/float(hist.sum())
-# plt.bar(bs[:-1], freq, width=0.25)
-# plt.xlim(-20,10)
-# 
-# =============================================================================
-# =============================================================================
-# l = [3,3,3,2,1,4,4,5,5,5,5,5]
-# print(len(l))
-# bins=np.arange(0,7,1)
-# print(bins)
-# h_test=plt.hist(l,density=True)
-# plt.show()
-# =============================================================================
+
 
 
 
