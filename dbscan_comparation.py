8174a44daa9f7021e4a7aa3f66c56837d3d228a9
Alvaro Martinez <amartinez@iaa.es>
2022-04-23 22:07:38 +0200

    Now look around all Ms stars in Libralato list. Also can chose cluster stars by pixel instean of coordenates. Generate list with clusters indivually.

--- dbscan_comparation.py
+++ dbscan_comparation.py
@@ -73,9 +73,6 @@ elif trimmed_data=='no':
 else:
     sys.exit("Have to set trimmed_data to either 'yes' or 'no'")
     
-cata='/Users/amartinez/Desktop/PhD/Libralato_data/CATALOGS/'
-pruebas='/Users/amartinez/Desktop/PhD/Libralato_data/pruebas/'
-results='/Users/amartinez/Desktop/PhD/Libralato_data/results/'
 # %%
 # We upload galactic center stars, that we will use in the CMD
 # catal=np.loadtxt(results+'refined_%s_PM.txt'%(name))
@@ -83,7 +80,23 @@ results='/Users/amartinez/Desktop/PhD/Libralato_data/results/'
 
 # "'RA_gns','DE_gns','Jmag','Hmag','Ksmag','ra','dec','x_c','y_c','mua','dmua','mud','dmud','time','n1','n2','ID','mul','mub','dmul','dmub','m139','Separation'",
 catal=np.loadtxt(results + '%smatch_GNS_and_%s_refined_galactic.txt'%(pre,name))
+# Definition of center can: m139 - Ks(libralato and GNS) or H - Ks(GNS and GNS)
+center_definition='G_G'#this variable can be L_G or G_G
+if center_definition =='L_G':
+    valid=np.where(np.isnan(catal[:,4])==False)# This is for the valus than make Ks magnitude valid, but shouldn´t we do the same with the H magnitudes?
+    catal=catal[valid]
+    center=np.where(catal[:,-2]-catal[:,4]>2.5) # you can choose the way to make the color cut, as they did in libralato or as it is done in GNS
+elif center_definition =='G_G':
+    valid=np.where((np.isnan(catal[:,3])==False) & (np.isnan(catal[:,4])==False ))
+    catal=catal[valid]
+    center=np.where(catal[:,3]-catal[:,4]>1.3)
+catal=catal[center]
+dmu_lim = 0.5
+vel_lim = np.where((catal[:,19]<=dmu_lim) & (catal[:,20]<=dmu_lim))
+catal=catal[vel_lim]
 
+# 'ra dec x_c  y_c mua dmua mud dmud time n1 n2 ID mul mub dmul dmub '
+catal_all = np.loadtxt(cata + '%s_pm_galactic.txt'%(name))
 
 
 
@@ -96,9 +109,10 @@ catal=np.loadtxt(results + '%smatch_GNS_and_%s_refined_galactic.txt'%(pre,name))
 
 # %%
 cluster_by='all'# this varible can be 'pm' or 'pos', indicating if you want cluster by velocities or positions,or all for clustering in 4D
+pixel = 'yes'# yes if you want coordenates in pixels for clustering and plotting positions, insteat of sky coordenates
 
 
-#mul, mub, mua, mud, ra, dec,dmul,dmub,l,b,Ks, H, m139, position in GALCEN_TABLE_D.cat 
+#mul, mub, mua, mud, ra, dec,dmul,dmub, position in GALCEN_TABLE_D.cat 
 Ms_all=np.loadtxt(pruebas +'pm_of_Ms_in_%s.txt'%(name))# this are the information (pm, coordinates and ID) for the Ms that remain in the data after triming it 
 group_lst=Ms_all[:,-1]#indtinfication number for the Ms
 
@@ -110,13 +124,15 @@ pms=[0,0,0,0]
 for file_to_remove in glob.glob(pruebas+'%scluster*.txt'%(pre)):#Remove the files for previpus runs adn radios
     os.remove(file_to_remove) 
 
-for g in range(len(group_lst)):
-# for g in range(0,2):
+# for g in range(len(group_lst)):
+for g in range(0,2):
     seed(g)
     fig, ax = plt.subplots(1,1,figsize=(30,10))
     ax.set_ylim(0,10)
     ax.text(0.0, 5, 'Group %s %s'%(int(group_lst[g]),pre),fontsize= 400,color=plt.cm.rainbow(random()))
-    
+    seed(g)
+    ax.text(0.5, 2, '\n'+'pixel=%s'%(pixel),fontsize= 200,color=plt.cm.rainbow(random()))
+
     # print(group_lst[g])
     samples=5# number of minimun objects that defined a cluster
     samples_dist = samples# the distance to the kth neightbour that will define the frist epsilon for debsacn to star looping
@@ -127,7 +143,7 @@ for g in range(len(group_lst)):
     r_u=[76]#this are the radios around the MS
 
     for r in  range(len(r_u)):
-      
+        # "'RA_gns','DE_gns','Jmag','Hmag','Ksmag','ra','dec','x_c','y_c','mua','dmua','mud','dmud','time','n1','n2','ID','mul','mub','dmul','dmub','m139','Separation'"
         data=np.loadtxt(pruebas + 'group_radio%s_%s_%s.txt'%(r_u[r],group,name))
         
         this=np.where(Ms_all[:,-1]==group)
@@ -142,7 +158,10 @@ for g in range(len(group_lst)):
         t_gal= QTable([gal_c.l,gal_c.b], names=('l','b'))  
         
     # %%
-        X=np.array([data[:,-6]-pms[2],data[:,-5]-pms[3],t_gal['l'].value,t_gal['b'].value]).T
+        if pixel == 'no':
+            X=np.array([data[:,-6]-pms[2],data[:,-5]-pms[3],t_gal['l'].value,t_gal['b'].value]).T
+        elif pixel == 'yes':
+            X=np.array([data[:,-6]-pms[2],data[:,-5]-pms[3],data[:,7],data[:,8]]).T
         if cluster_by == 'pos':
             X_stad = StandardScaler().fit_transform(X[:,[2,3]])
         elif cluster_by == 'pm':
@@ -217,6 +236,11 @@ for g in range(len(group_lst)):
             epsilon +=0.001 # if choose epsilon as min d_KNN you loop over epsilon and a "<" simbol goes in the while loop
             # samples +=1 # if you choose epsilon as codo, you loop over the number of sambles and a ">" goes in the  while loop
             print('DBSCAN loop %s. Trying with eps=%s. cluster = %s '%(loop,round(epsilon,3),len(set(l))-1))
+            if loop >100:
+                print('breaking out')
+                break
+               
+        # print('breaking the loop')
         print('This is the number of clusters: %s'%(len(set(l))-1))
         
         # =============================================================================
@@ -296,6 +320,12 @@ for g in range(len(group_lst)):
                 max_c=max(data[:,3][colores_index[i]]-data[:,4][colores_index[i]])
                 min_Ks=min(data[:,4][colores_index[i]])
                 if max_c-min_c <0.3:
+                    index1=np.where((catal[:,5]==Ms[0,4]) & (catal[:,6]==Ms[0,5]) ) # looping a picking the stars coord on the Ms catalog
+                    print(Ms[0,4],Ms[0,5])
+                    print(catal[:,5][index1],catal[:,6][index1])
+                    print(index1)
+                    # index=np.where((catal_all[:,0]==yso_ra[i]) & (catal_all[:,1]==yso_dec[i]) ) # this finding the MS in the whole libralati data, that is not trimmed, so its contains all the MS (well 96 of then the rest are in the other Libralato catalog)
+               
                     
                     fig, ax = plt.subplots(1,3,figsize=(30,10))
                     
@@ -304,9 +334,10 @@ for g in range(len(group_lst)):
                     ax[0].set_title('Group %s, radio = %s, # of Clusters = %s'%(group,r_u[r], n_clusters),color=plt.cm.rainbow(random()))
                     seed(g)
                     ax[1].set_title('# of stars = #%s, eps=%s'%(len(l),round(epsilon,3)),color=plt.cm.rainbow(random()))
-                    t_gal['l'] = t_gal['l'].wrap_at('180d')
+                    # t_gal['l'] = t_gal['l'].wrap_at('180d')
                     ax[0].scatter(X[:,0][colores_index[-1]],X[:,1][colores_index[-1]], color=colors[-1],s=50,zorder=1)
-                    ax[1].quiver(t_gal['l'][colores_index[-1]].value,t_gal['b'][colores_index[-1]].value, X[:,0][colores_index[-1]]-pms[2], X[:,1][colores_index[-1]]-pms[3], alpha=0.5, color=colors[-1])
+                    ax[0].scatter(X[:,0],X[:,1], color=colors[-1],s=50,zorder=1)
+                    # ax[1].quiver(t_gal['l'][colores_index[-1]].value,t_gal['b'][colores_index[-1]].value, X[:,0][colores_index[-1]]-pms[2], X[:,1][colores_index[-1]]-pms[3], alpha=0.5, color=colors[-1])
 
                     ax[0].scatter(X[:,0][colores_index[i]],X[:,1][colores_index[i]], color=colors[i],s=50,zorder=3)
                     ax[0].set_xlim(-10,10)
@@ -325,26 +356,61 @@ for g in range(len(group_lst)):
                     #data="'RA_gns','DE_gns','Jmag','Hmag','Ksmag','ra','dec','dmul','dmub','mua','dmua','mud','dmud','time','n1','n2','ID','mul','mub','dmul','dmub','m139','Separation'",
                     
                     # ax[1].scatter(data[:,0][colores_index[i]],data[:,1][colores_index[i]], color=colors[i],s=50)#plots in ecuatorials
-                    t_gal['l'] = t_gal['l'].wrap_at('180d')
-                    ax[1].scatter(t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value, color=colors[i],s=50,zorder=3)#plots in galactic
+                    if pixel =='no':
+                        t_gal['l'] = t_gal['l'].wrap_at('180d')
+                        ax[1].quiver(t_gal['l'][colores_index[-1]].value,t_gal['b'][colores_index[-1]].value, X[:,0][colores_index[-1]]-pms[2], X[:,1][colores_index[-1]]-pms[3], alpha=0.5, color=colors[-1])
+                        ax[1].scatter(t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value, color=colors[i],s=50,zorder=3)#plots in galactic
+                        ax[1].quiver(t_gal['l'].value,t_gal['b'].value, X[:,0]-pms[2], X[:,1]-pms[3], alpha=0.5, color=colors[-1])
+                        ax[1].scatter(t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value, color=colors[i],s=50,zorder=3)#plots in galactic           
+                        ax[1].quiver(t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value, X[:,0][colores_index[i]]-pms[2], X[:,1][colores_index[i]]-pms[3], alpha=0.5, color=colors[i])
+                        ax[1].set_xlabel('l') 
+                        ax[1].set_ylabel('b') 
+                        ax[1].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+                        ax[1].xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+                        if len(index1[0]) > 0:
+                            # mul, mub, mua, mud, ra, dec,dmul,dmub, position in GALCEN_TABLE_D.cat 
+                            Ms_co = SkyCoord(ra = Ms[0,4]*u.deg, dec = Ms[0,5]*u.deg, frame ='icrs').galactic
+                            ax[1].scatter(Ms_co.l.wrap_at('180d'), Ms_co.b,s=50, color='red', marker='2')
+                        else:
+                            Ms_co = SkyCoord(ra = Ms[0,4]*u.deg, dec = Ms[0,5]*u.deg, frame ='icrs').galactic
+                            ax[1].scatter(Ms_co.l.wrap_at('180d'), Ms_co.b,color='red',s=50,marker='o', facecolors='none', edgecolors='r')
+                    else:
+                        ax[1].scatter(X[:,2], X[:,3], color=colors[-1],s=50,zorder=3)#plots in galactic
+                        ax[1].quiver(X[:,2], X[:,3], X[:,0]-pms[2], X[:,1]-pms[3], alpha=0.5, color=colors[-1],zorder=1)
+                        ax[1].quiver(X[:,2][colores_index[-1]], X[:,3][colores_index[-1]], X[:,0][colores_index[-1]]-pms[2], X[:,1][colores_index[-1]]-pms[3], alpha=0.5, color=colors[-1])
+                        ax[1].scatter(X[:,2][colores_index[i]], X[:,3][colores_index[i]], color=colors[i],s=50,zorder=3)#plots in galactic
+                        ax[1].quiver(X[:,2][colores_index[i]], X[:,3][colores_index[i]], X[:,0][colores_index[i]]-pms[2], X[:,1][colores_index[i]]-pms[3], alpha=0.5, color=colors[i])
+                        ax[1].set_xlabel('x') 
+                        ax[1].set_ylabel('y') 
+                        ax[1].yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+                        ax[1].xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+                        if len(index1[0]) > 0:
+                            # mul, mub, mua, mud, ra, dec,dmul,dmub,x,y position in GALCEN_TABLE_D.cat 
+                            ax[1].scatter(Ms[0,-3], Ms[0,-2],s=50, color='red', marker='2')
+                        else:
+                            ax[1].scatter(Ms[0,-3], Ms[0,-2],color='red',s=50,marker='o', facecolors='none', edgecolors='r')
+                    
             #Ms=mul, mub, mua, mud, ra, dec,dmul,dmub,l,b,Ks, H, m139, position in GALCEN_TABLE_D.cat 
         
-                    ax[1].scatter(Ms[0,8],Ms[0,9],s=100,color='red',marker='2')
-                    ax[1].quiver(t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value, X[:,0][colores_index[i]]-pms[2], X[:,1][colores_index[i]]-pms[3], alpha=0.5, color=colors[i])
+                    # ax[1].scatter(Ms[0,8],Ms[0,9],s=100,color='red',marker='2')
                     # ax[1].quiver(data[:,17][colores_index[i]], data[:,18][colores_index[i]], X[:,0][colores_index[i]], X[:,1][colores_index[i]], alpha=0.5, color=colors[i])#galactic
             
                     # ax[1].set_xlabel('ra') 
                     # ax[1].set_ylabel('dec') 
                     
-                    ax[1].set_xlabel('l') 
-                    ax[1].set_ylabel('b') 
                     
-                    ax[1].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-                    ax[1].xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-                    np.savetxt(pruebas + '%scluster%s_of_group%s.txt'%(pre,i,g),np.array([data[:,5][colores_index[i]],data[:,6][colores_index[i]],t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value,
-                                                                               X[:,0][colores_index[i]], 
-                                                                               X[:,1][colores_index[i]],
-                                                                               data[:,3][colores_index[i]],data[:,4][colores_index[i]]]).T,fmt='%.7f', header ='ra, dec, l, b, pml, pmb, H, Ks')
+                    
+                    
+                    if pixel == 'no':
+                        np.savetxt(pruebas + '%scluster%s_of_group%s.txt'%(pre,i,g),np.array([data[:,5][colores_index[i]],data[:,6][colores_index[i]],t_gal['l'][colores_index[i]].value,t_gal['b'][colores_index[i]].value,
+                                                                                              X[:,0][colores_index[i]], 
+                                                                                              X[:,1][colores_index[i]],
+                                                                                              data[:,3][colores_index[i]],data[:,4][colores_index[i]]]).T,fmt='%.7f', header ='ra, dec, l, b, pml, pmb, H, Ks')
+                    elif pixel == 'yes':
+                        np.savetxt(pruebas + '%scluster%s_of_group%s.txt'%(pre,i,g),np.array([data[:,5][colores_index[i]],data[:,6][colores_index[i]],data[:,7][colores_index[i]], data[:,8][colores_index[i]],
+                                                                                              X[:,0][colores_index[i]], 
+                                                                                              X[:,1][colores_index[i]],
+                                                                                              data[:,3][colores_index[i]],data[:,4][colores_index[i]]]).T,fmt='%.7f', header ='ra, dec, x, y, pml, pmb, H, Ks')
                     
                     # %%'ra','dec','x_c','y_c','mua','dmua','mud','dmud','time','n1','n2','idt','m139','Separation','Ks','H'
                     # "'RA_gns','DE_gns','Jmag','Hmag','Ksmag','ra','dec','x_c','y_c','mua','dmua','mud','dmud','time','n1','n2','ID','mul','mub','dmul','dmub','m139','Separation'",
@@ -359,10 +425,12 @@ for g in range(len(group_lst)):
                     
                     p.arrow(max_c+max_c/5,min_Ks+0.5,-(max_c+max_c/5-max_c),0,head_width=0.05,color=colors[i])
                     ax[2].scatter(data[:,3][colores_index[i]]-data[:,4][colores_index[i]],data[:,4][colores_index[i]], color=colors[i],s=50,zorder=2)
+                    if len(index1[0]) > 0:
+                            ax[2].scatter((catal[:,3][index1]-catal[:,4][index1]),catal[:,4][index1], color='red',s=100,marker='2',zorder=3)
                     ax[2].axvline(min_c,color=colors[i],ls='dashed',alpha=0.5)
                     ax[2].axvline(max_c,color=colors[i],ls='dashed',alpha=0.5)
                     ax[2].annotate('%s'%(round(max_c-min_c,3)),(max_c+max_c/5,min_Ks+0.5),color=colors[i])
-                    ax[2].scatter((Ms[0,11]-Ms[0,10]),Ms[0,10], color='red',s=100,marker='2',zorder=3)
+                    # ax[2].scatter((Ms[0,11]-Ms[0,10]),Ms[0,10], color='red',s=100,marker='2',zorder=3)
                     ax[2].set_xlim(0,)
                     ax[2].set_xlabel('H$-$Ks') 
                     ax[2].set_ylabel('Ks') 
@@ -371,5 +439,5 @@ for g in range(len(group_lst)):
 
 
 # %%
-
+print(len(index1[0]))
 
