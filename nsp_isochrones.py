9e684f928cc13bfd798131128140767b88d41c30
Alvaro Martinez <amartinez@iaa.es>
2022-05-25 09:29:13 +0200

    Added more ages

--- nsp_isochrones.py
+++ nsp_isochrones.py
@@ -17,9 +17,11 @@ import matplotlib.pyplot as plt
 # In[5]:
 
 
-AKs = [1.6, 1.65, 1.7,1.75,1.8,1.85,1.9,1.95,2.0,2.05,2.10] # extinction in mags
+# AKs = [1.6, 1.65, 1.7,1.74,1.75,1.8,1.85,1.9,1.95,2.0,2.05,2.10] # extinction in mags
+AKs = np.arange(1.6,2.11,0.01)
+# AKs = [0]
 dist = 8000 # distance in parsec
-metallicity = 0.30 # Metallicity in [M/H]
+metallicity = [0.30,0.17] # Metallicity in [M/H]
 
 # Define evolution/atmosphere models and extinction law
 evo_model = evolution.MISTv1() 
@@ -34,13 +36,14 @@ filt_list = ['hawki,J', 'hawki,H', 'hawki,Ks']
 # table saved in. If the directory does not already exist,
 # SPISEA will create it.
 iso_dir = '/Users/amartinez/Desktop/PhD/Libralato_data/nsd_isochrones/'
-
+logAge = [np.log10(0.010*10**9.), np.log10(0.030*10**9.),np.log10(0.060*10**9.),np.log10(0.090*10**9.), np.log10(0.5*10**9.),np.log10(0.61*10**9.),np.log10(0.80*10**9.),np.log10(1*10**9.)]
 for a in AKs:
-    logAge = np.log10(0.61*10**9.) # Age in log(years)
-    iso_nsd = synthetic.IsochronePhot(logAge, a, dist, metallicity=metallicity,
-                                evo_model=evo_model, atm_func=atm_func,
-                                red_law=red_law, filters=filt_list,
-                                    iso_dir=iso_dir)
+    for b in logAge:
+        for c in metallicity:
+            iso_nsd = synthetic.IsochronePhot(b, a, dist, metallicity=c,
+                                    evo_model=evo_model, atm_func=atm_func,
+                                    red_law=red_law, filters=filt_list,
+                                        iso_dir=iso_dir)
 
 
 # In[ ]:
