7d8da44b95411bbd073af1736fa2401c92c14546
Alvaro Martinez <amartinez@iaa.es>
2022-07-21 11:22:28 +0200

    Added some  more isochornes and ages

--- nsp_isochrones.py
+++ nsp_isochrones.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 # AKs = [1.6, 1.65, 1.7,1.74,1.75,1.8,1.85,1.9,1.95,2.0,2.05,2.10] # extinction in mags
 AKs = np.arange(1.6,2.11,0.01)
 # AKs = [0]
-dist = 8000 # distance in parsec
+dist = 8200 # distance in parsec
 metallicity = [0.30,0.17] # Metallicity in [M/H]
 
 # Define evolution/atmosphere models and extinction law
@@ -36,7 +36,9 @@ filt_list = ['hawki,J', 'hawki,H', 'hawki,Ks']
 # table saved in. If the directory does not already exist,
 # SPISEA will create it.
 iso_dir = '/Users/amartinez/Desktop/PhD/Libralato_data/nsd_isochrones/'
-logAge = [np.log10(0.010*10**9.), np.log10(0.030*10**9.),np.log10(0.060*10**9.),np.log10(0.090*10**9.), np.log10(0.5*10**9.),np.log10(0.61*10**9.),np.log10(0.80*10**9.),np.log10(1*10**9.)]
+# logAge = [np.log10(0.010*10**9.), np.log10(0.030*10**9.),np.log10(0.060*10**9.),np.log10(0.090*10**9.), np.log10(0.5*10**9.),np.log10(0.61*10**9.),np.log10(0.80*10**9.),np.log10(1*10**9.)]
+logAge = [np.log10(0.0025*10**9.),np.log10(0.0048*10**9.),np.log10(0.0015*10**9.),np.log10(0.0055*10**9.),np.log10(0.0020*10**9.)]
+
 for a in AKs:
     for b in logAge:
         for c in metallicity:
