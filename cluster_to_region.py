fcf7c1572a890d11f0c6c438044b91a4e7251878
Alvaro Martinez <amartinez@iaa.es>
2022-07-21 11:21:31 +0200

    Lots of changes, dont remember of I checked these out

--- cluster_to_region.py
+++ cluster_to_region.py
@@ -32,9 +32,11 @@ results='/Users/amartinez/Desktop/PhD/Libralato_data/results/'
 clusters = '/Users/amartinez/Desktop/PhD/Libralato_data/clusters_sections/'
 name='WFC3IR'
 
-# ra, dec, l, b, pml, pmb, H, Ks,cluster
-c_num = [192]
-eps_num= [0.122]
+# ra, dec, l, b, pml, pmb,J, H, Ks,x, y, Aks_mean, dAks_mean, radio("),cluster_ID
+cluster ='/Users/amartinez/Desktop/PhD/Libralato_data/pruebas/Sec_A_clus/cluster_num1_0_knn9_area2.12/cluster1_1_4_knn_9_area_2.12.txt'
+ID = np.loadtxt(cluster, unpack=True, usecols=(-1))
+c_num = int(ID[0])
+
 section = 'A'
 # ra, dec, l, b, mul, mub, H, Ks,cluster = np.loadtxt(clusters + 'Sec_%s_WFC3IR_cluster%s_eps%s.txt'%(section, clus, eps),unpack=True)
 
@@ -45,33 +47,30 @@ section = 'A'
 #     os.remove(file_to_remove)
 
 
-for eps in eps_num:
-    for clus in c_num:
-        eps = round(eps,3)
-        print(clus, eps)
-        ra, dec, l, b, mul, mub = np.loadtxt('/Users/amartinez/Desktop/morralla/cluster29_2_1_knn_7_area_4.77.txt',unpack=True,usecols=(0,1,2,3,4,5))
-        mul =(mul+5.72)
-        mub = mub +0.2
-        with open(pruebas+ 'reg%s_clus%s_eps%s.reg'%(section, clus,eps), 'w') as f:
-            f.write('# Region file format: DS9 version 4.1'+"\n"+'global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1'+"\n"+'galactic'+'\n')
-            f.close
-            
-        for i in range(len(ra)):
-            with open(pruebas+ 'reg%s_clus%s_eps%s.reg'%(section, clus,eps), 'a') as f:
-                print(mul[0],mub[0])
-                if mul[0]>0 and mub[0]>0:
-                    f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,180-math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
-                    print('ssss')
-                elif mul[0]<0 and mub[0]>0:
-                    f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,-1*math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
-                    print(mul[i],mub[i],-1*math.degrees(math.atan(mub[i]/mul[i])))
-                elif mul[0]>0 and mub[0]<0:
-                    f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,180 + (-1)*math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
-                    print('ssss')
-                elif mul[0]<0 and mub[0]<0:
-                    print('ssss')
-                    f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
-        f.close
+
+ra, dec, l, b, mul, mub = np.loadtxt(cluster,unpack=True,usecols=(0,1,2,3,4,5))
+mul =(mul+5.72)
+mub = mub +0.2
+with open(pruebas+ 'reg%s_clus%s.reg'%(section, c_num), 'w') as f:
+    f.write('# Region file format: DS9 version 4.1'+"\n"+'global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1'+"\n"+'galactic'+'\n')
+    f.close
+    
+for i in range(len(ra)):
+    with open(pruebas+ 'reg%s_clus%s.reg'%(section, c_num), 'a') as f:
+        print(mul[0],mub[0])
+        if mul[0]>0 and mub[0]>0:
+            f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,180-math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
+            print('ssss')
+        elif mul[0]<0 and mub[0]>0:
+            f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,-1*math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
+            print(mul[i],mub[i],-1*math.degrees(math.atan(mub[i]/mul[i])))
+        elif mul[0]>0 and mub[0]<0:
+            f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,180 + (-1)*math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
+            print('ssss')
+        elif mul[0]<0 and mub[0]<0:
+            print('ssss')
+            f.write('\n'.join(('point(%s,%s) # point=x'%(float(l[i]),float(b[i])),'# vector(%s,%s,%s",%s)'%(float(l[i]),float(b[i]),np.sqrt(mul[i]**2+mub[i]**2)*10,math.degrees(math.atan(mub[i]/mul[i]))),'\n')))   
+f.close
         
        
  
