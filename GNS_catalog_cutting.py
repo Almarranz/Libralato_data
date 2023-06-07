a762352676790def5e45b4a181a4e4e7ca3a38a0
Alvaro Martinez <amartinez@iaa.es>
2023-06-07 16:37:55 +0200

    Added also Quintuplet cluster

--- GNS_catalog_cutting.py
+++ GNS_catalog_cutting.py
@@ -37,11 +37,17 @@ gns_pd = pd.read_csv(cata + 'GNS_central.csv')# tCentral region of GNS
 # %
 gns = gns_pd.to_numpy()
 # %%
+choosen_cluster = 'quintuplet'
 gns_coord = SkyCoord(ra = gns[:,2], dec = gns[:,4],unit = 'degree')
-m_arc = SkyCoord(ra = [266.46081294], dec = [-28.82303557],unit = 'degree' )
+
+if choosen_cluster =='arches':
+    m_arc = SkyCoord(ra = [266.46081294], dec = [-28.82303557],unit = 'degree' )
+elif choosen_cluster == 'quintuplet':
+    m_arc = SkyCoord(['17h46m14.68579s'], ['-28d49m38.99169s'], equinox = 'J2000')
+
 idxc, around_arc, d2d,d3d =  ap_coor.search_around_sky(m_arc, gns_coord, 80*u.arcsec)
 gns_ar = gns[around_arc]
-np.savetxt(cata +'GNS_arches.txt', gns_ar, fmt = '%.8f', header = '_RAJ2000 _DEJ2000 RAJ2000 e_RAJ2000 DEJ2000 e_DEJ2000 RAJdeg e_RAJdeg DEJdeg e_DEJdeg RAHdeg e_RAHdeg DEHdeg e_DEHdeg RAKsdeg e_RAKsdeg DEKsdeg e_DEKsdeg mag e_Jmag Hmag e_Hmag Ksmag e_Ksmag iJ iH iKs')
+np.savetxt(cata +'GNS_%s.txt'%(choosen_cluster), gns_ar, fmt = '%.8f', header = '_RAJ2000 _DEJ2000 RAJ2000 e_RAJ2000 DEJ2000 e_DEJ2000 RAJdeg e_RAJdeg DEJdeg e_DEJdeg RAHdeg e_RAHdeg DEHdeg e_DEHdeg RAKsdeg e_RAKsdeg DEKsdeg e_DEKsdeg mag e_Jmag Hmag e_Hmag Ksmag e_Ksmag iJ iH iKs')
 
 
 
