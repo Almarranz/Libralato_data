2d389a1a128dc02b495d4e4de3cd347ef1d911b1
Alvaro Martinez <amartinez@iaa.es>
2024-03-07 10:50:38 +0100

    Something funny is going on with the aa transfomation matrix. I going to try passing the coordenates to xy offsets

--- Hosek_clusters_vs_dbscan_gns_clusters.py
+++ Hosek_clusters_vs_dbscan_gns_clusters.py
@@ -110,16 +110,25 @@ hp = arches['Pclust']>0.1
 arches = arches[hp]
 RA_DEC = center_arc.spherical_offsets_by(arches['dRA'], arches['dDE'])
 
-lim_radec = np.where((RA_DEC.ra.value<max(gns_clus[:,0]))&(RA_DEC.ra.value>min(gns_clus[:,0]))
-                  &(RA_DEC.dec.value<max(gns_clus[:,1]))&(RA_DEC.dec.value>min(gns_clus[:,1])))
+arches.add_column(RA_DEC.ra, name='RA', index=-1)
+arches.add_column(RA_DEC.dec, name='DEC', index=-1)
 
-RA_DEC = RA_DEC[lim_radec]
+# sys.exit()
+# lim_radec = np.where((RA_DEC.ra.value<max(gns_clus[:,0]))&(RA_DEC.ra.value>min(gns_clus[:,0]))
+#                   &(RA_DEC.dec.value<max(gns_clus[:,1]))&(RA_DEC.dec.value>min(gns_clus[:,1])))
+
+# RA_DEC = RA_DEC[lim_radec]
+clus_cen = SkyCoord(ra = [np.mean(gns_clus[:,0])], dec = [np.mean(gns_clus[:,1])], unit = 'degree')
+around_gns =  ap_coor.search_around_sky(clus_cen,RA_DEC,40*u.arcsec)
+
+arches = arches[around_gns[1]]
+RA_DEC = center_arc.spherical_offsets_by(arches['dRA'], arches['dDE'])
 
 
 # %
 around_ls = np.empty(0)
 ind_gns = np.empty(0)
-rad = 0.3*u.arcsec
+rad = .15*u.arcsec#!!!
 for i in range(len(gns_clus)):
 # for i in range(1):
     m_point = SkyCoord(ra = [gns_clus[i,0]], dec = [gns_clus[i,1]],
@@ -130,32 +139,40 @@ for i in range(len(gns_clus)):
     if len(group_md) >0:
         ind_gns = np.append(ind_gns,i).astype(int)
         
-    print(len(group_md))
+    # print(len(group_md))
 # %
 # import pandas as pd
 ar_uni = np.unique(around_ls).astype(int)
 
 fig, ax = plt.subplots(1,2)
 ax[0].scatter(RA_DEC.ra[ar_uni], RA_DEC.dec[ar_uni])
-# ax.scatter(gns_clus[:,0], gns_clus[:,1])
 ax[0].scatter(gns_clus[:,0][ind_gns], gns_clus[:,1][ind_gns])
+
 # ax.hist2d(RA, DEC, bins = 100)
 
-
-
-
-RA_DEC = RA_DEC[ar_uni]
+arches = arches[ar_uni]
+RA_DEC = center_arc.spherical_offsets_by(arches['dRA'], arches['dDE'])
 
 RA = RA_DEC.ra
 DEC = RA_DEC.dec
 
 lc1, lc2, l1 , l2 = compare_lists(np.array([RA.value,DEC.value]).T,gns_clus[ind_gns],0,1,0,1,0.3)
 
+color = ['r','g','k','y','b']# just checking the order
+# for i in range(5):
+#     ax[1].scatter(arches['RA'][l1][i], arches['DEC'][l1][i],color = color[i])
+#     ax[1].scatter(gns_clus[ind_gns][:,0][l2][i], gns_clus[ind_gns][:,1][l2][i],color = color[i])
 
-sys.exit()
+dpm_ra = arches['pmRA'][l1]-gns_clus[ind_gns][:,2][l2]
+dpm_dec = arches['pmDE'][l2]-gns_clus[ind_gns][:,3][l2]
+ax[1].hist(dpm_ra, histtype = 'step', label = '$\sigma \mu_{ra} = %.2f$'%(np.std(dpm_ra)))
+ax[1].hist(dpm_dec, histtype = 'step', label = '$\sigma \mu_{dec} = %.2f$'%(np.std(dpm_dec)))
+ax[1].legend()
+
+# sys.exit()
 # m,(_,_)= aa.find_transform(gns_clus[:,0:2],np.array([RA.value,DEC.value]).T,max_control_points=300)
 # m,(_,_)= aa.find_transform(gns_clus[:,0:2][ind_gns],np.array([RA.value,DEC.value]).T,max_control_points=50)
-m, (source_list, target_list)= aa.find_transform(gns_clus[:,0:2][ind_gns],np.array([RA.value,DEC.value]).T,max_control_points=50)
+m, (s_ls, t_ls)= aa.find_transform(gns_clus[:,0:2][ind_gns],np.array([RA.value,DEC.value]).T,max_control_points=50)
 
 print("Translation: (x, y) = (%.2f, %.2f)"%(m.translation[0],m.translation[1]))
 print("Rotation: %.1f degrees"%(m.rotation * 180.0 /np.pi)) 
@@ -165,13 +182,45 @@ print("Scale factor: %.4f"%(m.scale))
 
 gns_copy = copy.deepcopy(gns_clus[:,0:2][ind_gns])
 gns_aa = aa.matrix_transform(gns_copy, m)
+# gns_clus[:,0:2] = aa.matrix_transform(gns_clus[:,0:2], m)
 
 
+fig, ax = plt.subplots(1,2)
+ax[0].scatter(RA, DEC)
+# ax.scatter(gns_clus[:,0], gns_clus[:,1])
+ax[1].scatter(gns_aa[:,0],gns_aa[:,1], color = '#ff7f0e')
 fig, ax = plt.subplots(1,1)
 ax.scatter(RA, DEC)
 # ax.scatter(gns_clus[:,0], gns_clus[:,1])
 ax.scatter(gns_aa[:,0],gns_aa[:,1])
 
+# for i in range(len(color)):
+#     ax.scatter(s_ls[i][0], s_ls[i][1], color =color[i])
+# # ax.scatter(gns_clus[:,0], gns_clus[:,1])
+#     ax.scatter(t_ls[i][0],t_ls[i][1], color = color[i])
+# %%
 
+g_pmra = []
+h_pmra = []
+g_pmdec = []
+h_pmdec = []
+for i in range(len(s_ls)):
+    g_aa = (gns_clus[ind_gns][:,0] == s_ls[i][0]) & (gns_clus[ind_gns][:,1] == s_ls[i][1])
+    g_pmra.append(gns_clus[ind_gns][g_aa][0][2])
+    g_pmdec.append(gns_clus[ind_gns][g_aa][0][3])
+    
+    h_aa = (arches['RA'] == t_ls[i][0]) & (arches['DEC'] == t_ls[i][1])
+    h_pmra.append(arches['pmRA'][h_aa].value)
+    h_pmdec.append(arches['pmDE'][h_aa].value)
+    # print(gns_clus[ind_gns][g_aa][0][3])
+    # print(arches['pmRA'][h_aa].value)
+
+df_pmra = np.array(h_pmra).flatten()-(np.array(g_pmra))
+df_pmdec = np.array(h_pmdec).flatten()-(np.array(g_pmdec))
+
+fig, ax = plt.subplots(1,1)
+ax.hist(df_pmra,histtype ='step', label = '$\sigma \mu_{ra} = %.2f$'%(np.std(df_pmra)))
+ax.hist(df_pmdec,histtype ='step', label = '$\sigma \mu_{dec} = %.2f$'%(np.std(df_pmdec)))
+ax.legend()    
 
 
